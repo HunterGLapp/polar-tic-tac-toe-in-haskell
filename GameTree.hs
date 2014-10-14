fc096db527a8d01c7f4514d883f5ef47a7c41d05
HunterGLapp <hunterlapp@gmail.com>
2014-10-13 20:59:39 -0600

    added a function to cut off a game tree at a certain level

--- GameTree.hs
+++ GameTree.hs
@@ -8,3 +8,12 @@ data RoseTree a = Node a [RoseTree a] deriving (Show, Eq, Ord)
 
 ticTacToeTree (board, status) = Node (board, status) (map ticTacToeTree (possibleNewStates board status))
 
+getRoot :: RoseTree a -> a
+getRoot (Node root _) = root
+
+getChildren :: RoseTree a -> [RoseTree a]
+getChildren (Node _ children) = children
+
+trunc :: Int -> RoseTree a -> RoseTree a
+trunc 0 x = Node (getRoot x) []
+trunc n rosetree = Node (getRoot rosetree) (map (trunc (n - 1)) (getChildren rosetree))
