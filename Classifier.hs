030e3fc852a3178af97807b2d695fd688fb5f6c2
HunterGLapp <hunterlapp@gmail.com>
2014-11-04 21:27:09 -0700

    added to Classifier

--- Classifier.hs
+++ Classifier.hs
@@ -7,7 +7,7 @@ data BoardPrediction = Win | Loss
 type Probability = Double
 
 argmax :: (Ord n) => (a -> n) -> [a] -> [a]
-argmax fn set = [x | x <- set, fn x >= maximum (map fn set)
+argmax fn set = [x | x <- set, fn x >= maximum (map fn set)]
 
 argmax1 :: (Ord n) => (a -> n) -> [a] -> a
 argmax1 fn set = head (argmax fn set)
@@ -17,8 +17,13 @@ classes = [Win, Loss]
 prior Win = 0.5
 prior Loss = 0.5
 
---classify :: (Board, Status) -> BoardPrediction
---classify board = argmax1 classes (
+classify :: (Board, Status) -> BoardPrediction
+classify (board, status) = argmax1 p_func classes 
 
 p_func :: BoardPrediction -> (Board, Status) -> Double
-p_func c (board, status) = (prior c) * product 
+p_func c (board, status) = (prior c) * (product (getProbList c (board, status)))
+
+getProbList :: BoardPrediction -> (Board, Status) -> [Double]
+getProbList Win (board, status) = replicate 48 0.5
+getProblist Loss (board, status) = replicate 48 0.5
+
